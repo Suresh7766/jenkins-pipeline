51c49ee5138cbb07defe96f11fae8fac47e50751
Suresh7766 <tirupatisuresh7766@gmail.com>
2022-04-29 19:30:35 +0530

    added success code

--- src/main/java/com/microservices/SpringBootJenkinsApplication.java
+++ src/main/java/com/microservices/SpringBootJenkinsApplication.java
@@ -1,12 +1,24 @@
 package com.microservices;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootJenkinsApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(SpringBootJenkinsApplication.class);
+
+	@PostConstruct
+	public void onInit() {
+		logger.info("Spring Boot Jenkins App Started!...");
+	}
+
 	public static void main(String[] args) {
+		logger.info("Spring Boot Jenkins App Executing!..");
 		SpringApplication.run(SpringBootJenkinsApplication.class, args);
 	}
 

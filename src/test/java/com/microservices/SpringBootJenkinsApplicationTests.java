f802120880535f0bace1b4a43d76a949a05455a6
Suresh7766 <tirupatisuresh7766@gmail.com>
2022-05-04 19:54:36 +0530

    3rd commit

--- src/test/java/com/microservices/SpringBootJenkinsApplicationTests.java
+++ src/test/java/com/microservices/SpringBootJenkinsApplicationTests.java
@@ -3,13 +3,18 @@ package com.microservices;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootJenkinsApplicationTests {
 
+	private static Logger logger = LoggerFactory.getLogger(SpringBootJenkinsApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		logger.info("SpringBootJenkinsApplicationTests executed!....");
 		assertEquals(true, true);
 	}
 
